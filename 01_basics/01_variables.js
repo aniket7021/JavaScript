a7caeabef69e9dd31df4965556573a959600c66a
aniket7021 <grandmaster.aniket.702@gmail.com>
2024-12-10 17:50:54 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 123456
+let accountEmail = "aniket.702@gmail.com"
+var accountPassword = "123456789"
+accountCity = "Ranchi"
+
+// accountId = 654321  // This is not allowed to do because accountId is constant variable and its value cannot be changed
+
+console.log(accountId);
+
+accountEmail = "ak@ak.com"
+accountPassword = "212121"
+accountCity = "Banglore"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
