214ea374020b6022b785f57703d4f0437d87fb6f
aniket7021 <grandmaster.aniket.702@gmail.com>
2024-12-11 20:51:20 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,10 @@
+// console.log(2 > 1)  // true
+// console.log(2 < 1)  // false
+// console.log(2 == 1) // false
+// console.log(2 >= 1) // true
+// console.log(2 != 1) //true
+
+// console.log("2" > 1) and console.log("02" > 1) are true bcz if there is any relational operator bw a number or a String, then the string is converted into a number if possible and  then comparison is done.
+// console.log("12abc" > 1); will give a false bcz when JavaScript tried to convert "12abc" into number, then resulted into NaN and NaN can neither be greater or less than a given number.
+console.log("abc" == undefined)
+
